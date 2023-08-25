1f385a022d82222789d4674b0f996d4a6826aa79
Em-rullah <139845586+Em-rullah@users.noreply.github.com>
2023-08-25 15:22:45 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const btnNavEl = document.querySelector(".mobile-nav-buttons");
-const bodyEl = document.body;
-
-btnNavEl.addEventListener("click", function () {
-  bodyEl.classList.toggle("nav-open");
-});
